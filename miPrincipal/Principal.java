d4ce2b67c95308a5d905a60e9c6ac7112c77ee30
Marco Antonio Felix Torrez <159277886+MarcoFelix13@users.noreply.github.com>
2024-02-13 15:02:56 +0000

    Acualizacion Clase Distancia

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,10 +1,13 @@
 package miPrincipal;
-
-
-
+// Proposito: Calcular la distancia a partir
+// de la velocidad y el tiempo
+// Autor:Marco Antonio Felix Torrez
+// Fecha: 12/feb/2024}
+import java.util.Scanner;
 public class Principal {
     
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo, soy Marco");
+    
+    public static void main(String args[]){
+        
     }
 }
